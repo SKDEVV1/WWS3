5389d1c15b880fa49eb007ec8bf640d0b447fe30
SKDEVV1 <sorianokhaile33@gmail.com>
2024-10-23 15:17:44 +0800

    dekuai.js

--- ws3/commands/dekuai.js
+++ ws3/commands/dekuai.js
@@ -1,31 +1,29 @@
-const Deku = require("dekuai")
+const Deku = require("dekuai");
 const deku = new Deku();
+const name = "dekuai";
 
 module.exports = {
   name,
   description: "Interact with dekuai",
-  async run ({ api, event, send, args }){
+  async run({ api, event, send, args }) {
     const prompt = args.join(" ");
-    if (!prompt) return send(`Please enter your question!
-    
-Example: ${api.prefix + name} what is love?`);
-    send("Please wait... 🔎");
-const q = "hi",
-    cid = "40", // (conversational id) 
-    model = "Cblackbox";
-  const response = await deku[model](q, cid)
-  return console.log(response) 
-}
+    if (!prompt) return send(`Please enter your question! 
 
-  if (!deku || !deku.data.status)
-    throw new Error();
-    send(`${gpt.data.result}
+Example: ${api.prefix + name} what is love?`);
+    
+    send("Please wait... 🔎");
+    try {
+      const q = prompt,
+            cid = "40", // (conversational id)
+            model = "Cblackbox";
+      const response = await deku[model](q, cid);
+      if (!response || !response.data.status) throw new Error();
+      send(`${response.data.result}
 
 🤖 `);
-    } catch(err){
+    } catch (err) {
       send(err.message || err);
       return;
     }
   }
 }
-main()
